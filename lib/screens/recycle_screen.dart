4cc770e3b966eb12f89ba8f7eddf4a737d39c292
Kevin Bowe <kevinbowe1999@gmail.com>
2020-02-02 02:27:38 -0500

    add recycle screen

--- lib/screens/recycle_screen.dart
+++ lib/screens/recycle_screen.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:recycling_app/models/item.dart';
+import 'package:recycling_app/models/recycle.dart';
 
 class RecycleScreen extends StatelessWidget {
   static const routeName = '/recycle-item';
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Recycle'),
+    final itemFound = ModalRoute.of(context).settings.arguments as Item;
+    Recycle recycleInfo = itemFound.recycleInfo;
+    return Column(
+      children: <Widget>[
+        Text(recycleInfo.type),
+        Column(
+        //   ...recycleInfo.instructions.map().forEach( (instruction) {
+        //     Text(instruction)
+        //   }
+        // ).toList()
+        children: recycleInfo.instructions.map((location){ 
+              return Text(
+                location
+              );}
+            ).toList(),
+        
+        
+        )
+      ],
     );
   }
 }
