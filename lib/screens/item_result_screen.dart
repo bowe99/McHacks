9875006c77dd3af4b7d12d9e45cfdbbb8175a09b
Kevin Bowe <kevinbowe1999@gmail.com>
2020-02-02 04:14:27 -0500

    Add reduce page

---
+++ lib/screens/item_result_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ItemResultScreen extends StatelessWidget {
+  static const routeName = '/item-results';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Results'),
+    );
+  }
+}
